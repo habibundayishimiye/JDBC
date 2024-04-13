4306aca6b84d19eb7d3dc9dc26d225222e07bb19
habibundayishimiye <habibundayishimiye@gmail.com>
2024-04-13 23:18:54 +0200

    Addition of file
    ddasdafsdsdvdfdfbdfbvbcx

--- dux/src/Main.java
+++ dux/src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
+        for (int i = 1; i <= 100; i++) {
             //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
             // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
             System.out.println("i = " + i);
---
+++ dux/src/edison.java
@@ -0,0 +1,5 @@
+public class edison {
+public static void main(String[] args) {
+    System.out.println("Hello Edison");
+}
+}
